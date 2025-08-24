ebca610bb6ab0afbfda61f1fefc34d31a0aae6dc
renson_montoya@soy.sena.edu.co <renson_montoya@soy.sena.edu.co>
2025-08-23 19:32:55 -0500

    Fetch archivo config.py datos estáticos

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+CONFIG = {
+    "capacidad_maxima": 10,
+    "tarifas": {
+        "carro": {"hora": 3000, "dia": 20000},
+        "moto": {"hora": 2000, "dia": 10000},
+    },
+    "umbral_dia": 6,
+}
